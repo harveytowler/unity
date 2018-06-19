fcba05f1d8f3254825a40452f4c583b0447bc22d
harveytowler <34345468+harveytowler@users.noreply.github.com>
2018-06-19 14:14:34 +0100

    hello world

---
+++ Assets/Scripts/Activator.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+
+    SpriteRenderer sr;
+    public KeyCode key;
+    bool active = false;
+    GameObject Note;
+    List<float> missedNotes;
+    // Use this for initialization
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        missedNotes = new List<float>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            if (active)
+            {
+                Destroy(Note);
+                AddScore();
+                StartCoroutine(Pressed());
+            }
+            else
+            {
+                missedNotes.Add(UnityEngine.Time.fixedTime);
+
+            }
+        }
+    }
+
+        void OnTriggerEnter2D(Collider2D col)
+        {
+            active = true;
+            if (col.gameObject.tag == "Note")
+                Note = col.gameObject;
+
+        }
+
+
+        void OnTriggerExit2D(Collider2D col)
+        {
+            active = false;
+        }
+
+
+        void AddScore()
+        {
+            //PlayerPrefs.SetInt("Score",PlayerPrefs.GetInt("Score")+100);
+            GameObject.Find("Score").GetComponent<PPtext>().score += 100;
+        }
+
+        IEnumerator Pressed()
+        {
+            Color old = sr.color;
+            sr.color = new Color(0, 0, 0);
+            yield return new WaitForSeconds(0.2f);
+            sr.color = old;
+        }
+
+    }
---
+++ Assets/Scripts/PPtext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PPtext : MonoBehaviour {
+
+    public Text scoreName;
+    public int score = 000000;
+    
+    void Update () {
+        scoreName = GameObject.Find("Score").GetComponent<Text>();
+        scoreName.text = "Score: " + score;
+    }
+}
